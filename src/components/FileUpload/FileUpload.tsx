62bc6133445a260233980311e2643cfcdbf92b5b
theshushant <malikshushant420@gmail.com>
2023-03-28 22:25:54 +0530

    compress file donw

--- src/components/FileUpload/FileUpload.tsx
+++ src/components/FileUpload/FileUpload.tsx
@@ -1,4 +1,5 @@
 import React, { useRef } from "react";
+import imageCompression from "browser-image-compression";
 interface  Props {
     child:React.ReactNode,
     onSelect: Function,
@@ -6,16 +7,29 @@ interface  Props {
 const FileUpload:React.FC<Props> = (props) => {
     const inputFile:React.RefObject<HTMLInputElement> = useRef(null);
 
-    const handleFileUpload = (e:any) => {
-        const { files } = e.target;
+    const handleFileUpload = async (e: any) => {
+        const {files} = e.target;
         if (files && files.length) {
             const filename = files[0].name;
 
             var parts = filename.split(".");
             const fileType = parts[parts.length - 1];
             console.log("fileType", fileType); //ex: zip, rar, jpg, svg etc.
+            const options = {
+                maxSizeMB: 1,
+                maxWidthOrHeight: 1920,
+                useWebWorker: true
+            }
+            try {
+                const compressedFile = await imageCompression(files[0], options);
+                console.log('compressedFile instanceof Blob', compressedFile instanceof Blob); // true
+                console.log(`compressedFile size ${compressedFile.size / 1024 / 1024} MB`); // smaller than maxSizeMB
 
-            props.onSelect(files[0]);
+                props.onSelect(compressedFile); // write your own logic
+            } catch (error) {
+                console.log(error);
+            }
+
         }
     };
 
